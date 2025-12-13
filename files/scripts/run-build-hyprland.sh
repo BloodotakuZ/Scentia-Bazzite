87c34989e217d22cd68653ab372a373bcbd88726
BloodotakuZ <scentia14@proton.me>
2025-12-13 01:46:52 +0000

    Add files via upload

--- files/scripts/run-build-hyprland.sh
+++ files/scripts/run-build-hyprland.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
+# Ensure script is executable (defensive in case mode got lost)
+chmod +x /usr/local/bin/build-hyprland.sh
 /usr/local/bin/build-hyprland.sh
